8d4c70f7545997e2457cb9165b81a2dae11067b1
Handyfon <Handyfon@hotmail.com>
2020-02-20 17:46:10 +0100

    1.0.2
    1.0.2

--- js/character.js
+++ js/character.js
@@ -114,8 +114,8 @@ export class ActorSheet5eCharacter extends ActorSheet5e {
 	if ( game.settings.get("darksheet", "slotbasedinventory") ) {
     for ( let i of items ) {
       i.data.quantity = i.data.quantity || 0;
-      i.data.flags.darksheet.item.slots = i.data.flags.darksheet.item.slots || 0;
-      i.totalWeight = Math.round(i.data.quantity * i.data.flags.darksheet.item.slots *10)/10;
+	  i.flags.darksheet.item.slots = getProperty(i, 'flags.darksheet.items.slots') || 10;  
+      i.totalWeight = Math.round(i.data.quantity * i.flags.darksheet.item.slots *10)/10;
       inventory[i.type].items.push(i);
       totalWeight += i.totalWeight;
     }
